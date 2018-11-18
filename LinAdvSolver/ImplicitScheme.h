a877d5770e9140b5c721481fc52959b706b57218
elPorino <julien6359@gmail.com>
2018-11-18 15:11:42 +0100

    Update ImplicitScheme.h

--- LinAdvSolver/ImplicitScheme.h
+++ LinAdvSolver/ImplicitScheme.h
@@ -3,8 +3,13 @@
 class ImplicitScheme :
 	public Scheme
 {
+protected:
+	vector<double> finalVector;
 public:
+	vector<double> thomasAlgo(double a, double b, double c, vector<double> vectorInit);
 	ImplicitScheme();
+	ImplicitScheme(Scheme mother);
 	~ImplicitScheme();
+	vector<double> get_finalVector();
 };
 
