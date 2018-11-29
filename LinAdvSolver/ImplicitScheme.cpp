80d65b5c62beb90a9275ae8c55921481d847aeed
elPorino <julien6359@gmail.com>
2018-11-29 12:32:30 +0100

    Update ImplicitScheme.cpp

--- LinAdvSolver/ImplicitScheme.cpp
+++ LinAdvSolver/ImplicitScheme.cpp
@@ -1,10 +1,15 @@
+/*!
+ * \file ImplicitScheme.cpp
+ * \brief Source file of The ImplicitScheme Class
+ * \author GARIKIPATI Bhargav and AMARIR Julien
+ * \version Final Version
+ */
+
 #include "ImplicitScheme.h"
-
-
 
 vector<double> ImplicitScheme::thomasAlgo(double a, double b, double c, vector<double> vectorInit)
 {
-	vector<double> vectorResult(nb_points+1);
+	vector<double> vectorResult(nb_points + 1);
 	double m = 0.0;
 	double bk = 0.0;
 
@@ -13,7 +18,7 @@ vector<double> ImplicitScheme::thomasAlgo(double a, double b, double c, vector<d
 	m = a / b;
 	bk = b - (m*c);
 
-	for (int k = 2; k < nb_points + 1; k++) {
+	for (int k = 1; k < nb_points + 1; k++) {
 
 		vecD[k] -= m * vecD[k - 1];
 
@@ -25,55 +30,34 @@ vector<double> ImplicitScheme::thomasAlgo(double a, double b, double c, vector<d
 		vectorResult[k] = (vecD[k] - (c*vectorResult[k + 1])) / bk;
 
 	}
-
-
-
 	return vectorResult;
 }
-
-ImplicitScheme::ImplicitScheme()
-{
-}
-
-ImplicitScheme::ImplicitScheme(Scheme mother) : Scheme(mother), finalVector(vector<double>(nb_points))
-{
-
-}
-
-
-ImplicitScheme::~ImplicitScheme()
-{
-}
-
-vector<double> ImplicitScheme::get_finalVector()
-{
-	return finalVector;
-}
-
-vector<vector<double>> ImplicitScheme::ftcsSolver() {
+/*!
+*  \brief Solve the problem using the implicit Upwind Scheme characteristics,
+*	modify the values of the next attribut : finalVector
+*	and return a vector composed by vectors of doubles one for time = 0.1, 0.2, 0.3, 0.4 and 0.5.
+*  \return vector of vectors of doubles
+*/
+vector<vector<double>> ImplicitScheme::solveUpwindImplicit() {
 	vector<vector<double>> finalResults;
-	baseVecCalculator();
-	double alpha = (u*delta_t) / (2.0*delta_x);
+	double alpha = (u*delta_t) / delta_x;
 	double a = -alpha;
-	double b = 1.0;
-	double c = alpha;
+	double b = 1.0 + alpha;
+	double c = 0.0;
 	vector<double> temp = baseVector;
+	finalResults.push_back(temp);
 	finalVector = thomasAlgo(a, b, c, temp);
 	finalVector[0] = 0.0;
 	finalVector[nb_points] = 0.0;
+	finalResults.push_back(finalVector);
+	current_t = delta_t;
 
-
-	while (current_t <= (t_max + (delta_t / 4.0))) {
-
+	while (current_t < (t_max + (delta_t / 4.0))) {
 		finalVector = thomasAlgo(a, b, c, temp);
-		
 		finalVector[0] = 0.0;
 		finalVector[nb_points] = 0.0;
 		temp = finalVector;
-		
-		
-		
-		
+
 		if ((current_t < (0.1 + (delta_t / 4.0)) && current_t >(0.1 - (delta_t / 4.0))) || (current_t < (0.2 + (delta_t / 4.0)) && current_t >(0.2 - (delta_t / 4.0))) || (current_t < (0.3 + (delta_t / 4.0)) && current_t >(0.3 - (delta_t / 4.0))) || (current_t < (0.4 + (delta_t / 4.0)) && current_t >(0.4 - (delta_t / 4.0))) || (current_t < (0.5 + (delta_t / 4.0)) && current_t >(0.5 - (delta_t / 4.0)))) {
 
 			finalResults.push_back(finalVector);
@@ -86,19 +70,25 @@ vector<vector<double>> ImplicitScheme::ftcsSolver() {
 
 	return finalResults;
 }
-
-vector<vector<double>> ImplicitScheme::upwindImplicitSolver() {
+/*!
+*  \brief Solve the problem using the implicit FTCS Scheme characteristics,
+*	modify the values of the next attribut : finalVector
+*	and return a vector composed by vectors of doubles one for time = 0.1, 0.2, 0.3, 0.4 and 0.5.
+*  \return vector of vectors of doubles
+*/
+vector<vector<double>> ImplicitScheme::solveFTCSImplicit() {
 	vector<vector<double>> finalResults;
-	baseVecCalculator();
-	double alpha = (u*delta_t) / delta_x;
+	double alpha = (u*delta_t) / (2.0*delta_x);
 	double a = -alpha;
-	double b = 1.0+alpha;
-	double c = 0.0;
+	double b = 1.0;
+	double c = alpha;
 	vector<double> temp = baseVector;
+	finalResults.push_back(temp);
 	finalVector = thomasAlgo(a, b, c, temp);
 	finalVector[0] = 0.0;
 	finalVector[nb_points] = 0.0;
-
+	current_t = delta_t;
+	
 
 	while (current_t < (t_max + (delta_t / 4.0))) {
 
@@ -108,13 +98,9 @@ vector<vector<double>> ImplicitScheme::upwindImplicitSolver() {
 		finalVector[nb_points] = 0.0;
 		temp = finalVector;
 
-
-
-
-		if ((current_t < (0.1 + (delta_t/4.0)) && current_t >(0.1 - (delta_t/4.0))) || (current_t < (0.2 + (delta_t/4.0)) && current_t >(0.2 - (delta_t/4.0))) || (current_t < (0.3 + (delta_t/4.0)) && current_t >(0.3 - (delta_t/4.0))) || (current_t < (0.4 + (delta_t/4.0)) && current_t >(0.4 - (delta_t/4.0))) || (current_t < (0.5 + (delta_t/4.0)) && current_t >(0.5 - (delta_t/4.0)))) {
+		if ((current_t < (0.1 + (delta_t / 4.0)) && current_t >(0.1 - (delta_t / 4.0))) || (current_t < (0.2 + (delta_t / 4.0)) && current_t >(0.2 - (delta_t / 4.0))) || (current_t < (0.3 + (delta_t / 4.0)) && current_t >(0.3 - (delta_t / 4.0))) || (current_t < (0.4 + (delta_t / 4.0)) && current_t >(0.4 - (delta_t / 4.0))) || (current_t < (0.5 + (delta_t / 4.0)) && current_t >(0.5 - (delta_t / 4.0)))) {
 
 			finalResults.push_back(finalVector);
-
 		}
 
 		current_t += delta_t;
@@ -122,4 +108,36 @@ vector<vector<double>> ImplicitScheme::upwindImplicitSolver() {
 	}
 
 	return finalResults;
+}
+
+/*!
+*  \brief Method to get the next vector : finalVector
+*
+*  \return the finalVector
+*/
+vector<double> ImplicitScheme::get_finalVector()
+{
+	return finalVector;
+}
+/*!
+ *  \brief Basic Constructor
+ *	Create an instance of the ImplicitScheme class.
+ */
+ImplicitScheme::ImplicitScheme()
+{
+}
+/*!
+*  \brief Constructor
+*	Create an instance of the ImplicitScheme class and set the values of delta_t, delta_x, t_max, analytical_t and current_t coming from an instance of the Scheme class.
+*
+*  \param mother : mother instance from which will .
+*
+*/
+ImplicitScheme::ImplicitScheme(Scheme mother) : Scheme(mother), finalVector(vector<double>(nb_points))
+{
+
+}
+
+ImplicitScheme::~ImplicitScheme()
+{
 }
